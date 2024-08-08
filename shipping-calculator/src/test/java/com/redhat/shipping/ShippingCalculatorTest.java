6b94ac47886b8b46e79cbbb60345ec6059d512b2
isnjng <isingmyself@gmail.com>
2024-08-08 12:05:14 -0400

    Refactor

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -6,5 +6,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
 
-    // @todo: add tests
+   @Test
+
+   public void onNARegionTheCostIs100() {
+		ShippingCalculator calculator = new ShippingCalculator();
+		int calculatedCost = calculator.costForRegion("NA");
+	      // assertEquals(100, (new ShippingCalculator()).costForRegion("NA"));
+		assertEquals(100, calculatedCost);
+   }
+
+   @Test
+
+   public void canCalculateTheCostForARegion() {
+
+	       ShippingCalculator calculator = new ShippingCalculator();
+
+
+
+	           assertEquals(0, calculator.costForRegion("A Region"));
+
+   }
+
+
+   @Test
+
+   public void onLATAMRegionTheCostIs200() {
+
+	       ShippingCalculator calculator = new ShippingCalculator();
+
+	       int calculatedCost = calculator.costForRegion("LATAM");
+		assertEquals(200, calculatedCost);
+
+   }
+
+   @Test
+
+   public void onEMEARegionTheCostIs300() {
+	       ShippingCalculator calculator = new ShippingCalculator();
+	        int calculatedCost = calculator.costForRegion("EMEA");
+		assertEquals(300, calculatedCost);
+   }
 }
